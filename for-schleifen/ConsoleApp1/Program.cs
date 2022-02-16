1b4ca5278b7d80110e8d1e8a8a5e6107640ffda3
vaporvee <80621863+cmod31@users.noreply.github.com>
2022-02-16 08:33:01 +0100

    Add files via upload

---
+++ for-schleifen/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            for (a = 10; a >= 0; a--) 
+            {
+                for (int i = a; i >= 0; i--)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine("");
+            }
+
+            Console.Read();
+        }
+    }
+}
